7d7e1c703bf105af71683ccda4eac40b66b7d14c
Joseph Stowe <joseph.stowe@mail.rakuten.com>
2017-12-05 22:17:07 +0900

    Adding test

--- src/test/java/game/db/FeedbackStoreTest.java
+++ src/test/java/game/db/FeedbackStoreTest.java
@@ -12,15 +12,13 @@ import redis.clients.jedis.JedisPool;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.HashMap;
+import java.util.*;
 
 public class FeedbackStoreTest {
 
     @Mock private Jedis jedis;
     @Mock private JedisPool pool;
+    private Map<String,String> hash1 = new HashMap<>();
 
     @Before
     public void setup() {
@@ -28,18 +26,16 @@ public class FeedbackStoreTest {
         RedisHelper.setPool(pool);
         Mockito.when(pool.getResource()).thenReturn(new Jedis());
         Mockito.when(RedisHelper.jedis()).thenReturn(jedis);
-    }
-
-    @Test
-    public void getRecent() {
-        int limit = ReviewResource.DEFAULT_LIMIT;
-
-        Map<String,String> hash1 = new HashMap<>();
         hash1.put("id", "101");
         hash1.put("rating", "5");
         hash1.put("comment", "I loved it!");
         hash1.put("userId", "user_409");
         hash1.put("sessionId", "session_2134");
+    }
+
+    @Test
+    public void getRecentWithDefaultRange() {
+        int limit = ReviewResource.DEFAULT_LIMIT;
 
         HashSet<String> resultKeys1 = new HashSet<>();
         String key1 = "Feedback:1";
@@ -48,8 +44,6 @@ public class FeedbackStoreTest {
         Mockito.when(jedis.zrevrange(RedisHelper.FEEDBACK_ID_INDEX_KEY, 0, limit -1))
                 .thenReturn(resultKeys1);
 
-        ArrayList<Feedback> list = FeedbackStore.getRecent(5, 1, 15);
-
         Mockito.verify(jedis, Mockito.never()).zrevrangeByLex(
                 Mockito.anyString(),
                 Mockito.anyString(),
@@ -57,6 +51,34 @@ public class FeedbackStoreTest {
                 Mockito.anyInt(),
                 Mockito.anyInt());
 
+        ArrayList<Feedback> list = FeedbackStore.getRecent(5, 1, 15);
+
+        assertThat(list.size() == 1);
+        Feedback feedback = list.get(0);
+        assertThat(feedback.getId() == 101);
+        assertThat(feedback.getRating() == 5);
+        assertThat(feedback.getComment().equals("I loved it!"));
+        assertThat(feedback.getUserId().equals("user_409"));
+        assertThat(feedback.getSessionId().equals("session_2134"));
+    }
+
+    @Test
+    public void getRecentWithSingleRating() {
+        int limit = ReviewResource.DEFAULT_LIMIT;
+
+        Set<String> resultMembers = new HashSet<>();
+        String member1 = "5:0000000012";
+        resultMembers.add(member1);
+        String key1 = "Feedback:12";
+        Mockito.doReturn(hash1).when(jedis).hgetAll(key1);
+        Mockito.when(jedis.zrevrangeByLex(
+                Mockito.anyString(),
+                Mockito.anyString(),
+                Mockito.anyString(),
+                Mockito.anyInt(),
+                Mockito.anyInt())).thenReturn(resultMembers);
+
+        ArrayList<Feedback> list = FeedbackStore.getRecent(5, 5, limit);
 
         assertThat(list.size() == 1);
         Feedback feedback = list.get(0);
